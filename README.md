aa2565cf369de0811b9281c78901f07c77dc6fc4
Trieu Le <lehuynhhaitrieu1996@gmail.com>
2026-02-21 12:40:06 -0700

    display ppm file

---
+++ README.md
@@ -0,0 +1,12 @@
+Dumb [PPM file](https://netpbm.sourceforge.net/doc/ppm.html)(P3 format specifically) parser 
+
+## Build
+```console
+gcc main.c -o main -Iraylib-5.5_macos/include -Lraylib-5.5_macos/lib -rpath raylib-5.5_macos/lib -lraylib 
+./main
+```
+
+## Example
+![Screenshot](screenshot.png)
+
+
--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 #include <errno.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <raylib.h>
 
 typedef struct Pixel {
   uint8_t r;
   uint8_t g;
   uint8_t b;
 } Pixel;
+
+#define WINDOW_SIZE 400
+#define PIXEL_SIZE 100
 
 int main()
 {
@@ -61,6 +65,21 @@ int main()
   }
 
   fclose(fp);
+
+  InitWindow(WINDOW_SIZE, WINDOW_SIZE, "PPM");
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+      ClearBackground(RAYWHITE);
+      for (size_t y = 0; y < h; y++) {
+        for (size_t x = 0; x < w; x++) {
+          Pixel p = pxs[y][x];
+          DrawRectangle(x*PIXEL_SIZE, y*PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, (Color) {p.r, p.g, p.b, 255});
+        }
+      }
+    EndDrawing();
+  }
+
+  CloseWindow();
   return 0;
 }
 
